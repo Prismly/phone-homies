32f56d8f0303ff129f7f3358f2468ff65c4db166
Prismly <prismly55@gmail.com>
2024-04-12 23:44:35 -0400

    Imported Symbols and Speech Bubble
    - Imported three symbol assets (saturn, star, rocket)
    - Imported speech bubble asset (pending replacement)

---
+++ Assets/Scripts/Jiggle.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//
+public class Jiggle : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
